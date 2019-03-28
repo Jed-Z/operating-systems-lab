f218605af596b20b173659a5da2f1babb2edbd30
Jed <jed@jeddd.com>
2019-03-28 22:21:27 +0800

    项目3：本项目v2终版

--- 项目3_开发独立内核的操作系统/src/libc.c
+++ 项目3_开发独立内核的操作系统/src/libc.c
@@ -19,6 +19,8 @@ extern uint8_t getUsrProgHead(uint16_t pid);
 extern uint8_t getUsrProgSector(uint16_t pid);
 extern uint16_t getUsrProgAddr(uint16_t pid);
 
+char* initcmd = "run 1 2 3 4";
+
 /* 系统启动界面 */
 void startUp() {
     clearScreen();
@@ -48,6 +50,7 @@ void showHelp() {
     "    clear - clear the terminal screen\r\n"
     "    list - show a list of user programmes and their PIDs\r\n"
     "    run <PIDs> - run user programmes in sequence, e.g. `run 3 2 1`\r\n"
+    "    sh <file> - run a script file, e.g. `sh init.cmd`\r\n"
     "    poweroff - force shutdown the machine\r\n"
     ;
     print(help_msg);
@@ -73,6 +76,7 @@ void listUsrProg() {
         NEWLINE;
     }
 }
+
 /* 操作系统shell */
 void shell() {
     clearScreen();
@@ -80,8 +84,8 @@ void shell() {
     
     char cmdstr[BUFLEN+1] = {0};  // 用于存放用户输入的命令和参数
     char cmd_firstword[BUFLEN+1] = {0};  // 用于存放第一个空格之前的子串
-    enum command       { help,   clear,   list,   run,   poweroff };
-    char* commands[] = {"help", "clear", "list", "run", "poweroff"};
+    enum command       { help,   clear,   list,   run,   sh,   poweroff };
+    char* commands[] = {"help", "clear", "list", "run", "sh", "poweroff"};
 
     while(1) {
         promptString();
@@ -124,6 +128,41 @@ void shell() {
                 print(error_msg);
             }
 
+        }
+        else if(strcmp(cmd_firstword, commands[sh]) == 0) {
+            char* knownfile = "init.cmd";
+            char scriptfile[BUFLEN+1], pids[BUFLEN+1];
+            getAfterFirstWord(cmdstr, scriptfile);
+            if(strcmp(scriptfile, knownfile)!=0) {
+                char* errorfile = "Script file does not exist.\r\n";
+                print(errorfile);
+                continue;
+            }
+
+            getAfterFirstWord(initcmd, pids);  // 获取run后的参数列表
+            uint8_t isvalid = 1;  // 参数有效标志位
+            for(int i = 0; pids[i]; i++) {  // 判断参数是有效的
+                if(!isnum(pids[i]) && pids[i]!=' ') {  // 及不是数字又不是空格，无效参数
+                    isvalid = 0;
+                    break;
+                }
+            }
+            if(isvalid) {  // 参数有效，则按顺序执行指定的用户程序
+                int i = 0;
+                for(int i = 0; pids[i] != '\0'; i++) {
+                    if(isnum(pids[i])) {  // 是数字（不是空格）
+                        int pid_to_run = pids[i] - '0';  // 要运行的用户程序PID
+                        loadAndRun(getUsrProgCylinder(pid_to_run), getUsrProgHead(pid_to_run), getUsrProgSector(pid_to_run), getUsrProgSize(pid_to_run)/512, getUsrProgAddr(pid_to_run));
+                        clearScreen();
+                    }
+                }
+                char* hint = "All programmes have been executed successfully as you wish.\r\n";
+                print(hint);
+            }
+            else {  // 参数无效，报错，不执行任何用户程序
+                char* error_msg = "Invalid arguments. PIDs must be decimal numbers.\r\n";
+                print(error_msg);
+            }
         }
         else if(strcmp(cmd_firstword, commands[poweroff]) == 0) {
             powerOff();
